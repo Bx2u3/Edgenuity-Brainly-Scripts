3551e5d1ce6a8a2441efd2f08b7cb4f5c2acf3d4
Subatomicmc <46378608+Subatomicmc@users.noreply.github.com>
2020-10-11 12:36:21 -0500

    Update VideoWatcher.user.js

--- VideoWatcher.user.js
+++ VideoWatcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Edgenuity Video Watcher
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.1.0
 // @description  Automates watching videos
 // @author       Subatomicmc
 // @match        https://student.edgenuity.com/enrollment/*/coursemap
